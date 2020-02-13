46cd11dd48eab731b07429c0038f4cf488fd2daa
Ambre Margerit <ambremgt@gmail.com>
2020-02-13 18:26:23 +0100

    MVC ok

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Cocktail.destroy_all
+Ingredient.destroy_all
+Dose.destroy_all
+
+puts 'Creating categories...'
+
+gin = Cocktail.create!(name: "Gin To")
+sunrise = Cocktail.create!(name: "Tequila Sunrise")
+caipirina = Cocktail.create!(name: "caipirina")
+
+lemon = Ingredient.create(name: "lemon")
+ice = Ingredient.create!(name: "ice")
+menthol = Ingredient.create!(name: "menthol")
+salt = Ingredient.create!(name: "salt")
+tequila = Ingredient.create!(name: "tequila")
+perrier = Ingredient.create!(name: "perrier")
+
+Dose.create!(description: "12cl", cocktail_id: gin.id, ingredient_id: lemon.id)
+Dose.create!(description: "9cl", cocktail_id: sunrise.id, ingredient_id: tequila.id)
+Dose.create!(description: "9cl", cocktail_id: caipirina.id, ingredient_id: caipirina.id)
+
+puts 'Finished!'
